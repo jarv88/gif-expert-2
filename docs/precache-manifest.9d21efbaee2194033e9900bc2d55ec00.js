399d480231f04e95b2de14985ee0ed402bfea950
jarv88 <joseangel038@gmail.com>
2020-10-16 15:38:33 -0500

    index html modificado

---
+++ docs/precache-manifest.9d21efbaee2194033e9900bc2d55ec00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9003c08bfa4be3cad3e8440ab66d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e782ce46f12790f36cb",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1c0de6511358a13a4365",
+    "url": "/static/js/2.8a98e3df.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.8a98e3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e782ce46f12790f36cb",
+    "url": "/static/js/main.773cb48f.chunk.js"
+  },
+  {
+    "revision": "adc2915afdca3ee77697",
+    "url": "/static/js/runtime-main.b5c3e8cf.js"
+  }
+]);
